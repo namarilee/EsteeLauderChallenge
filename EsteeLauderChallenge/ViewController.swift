692dcc262e9cf6231cffb64ae9ec6a9ab28ac03d
Marina Lee <leemarina@gmail.com>
2021-07-20 11:45:32 -0700

    animate button

--- EsteeLauderChallenge/ViewController.swift
+++ EsteeLauderChallenge/ViewController.swift
@@ -14,7 +14,22 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
+    
+    
 
-
+    @IBAction func browseButtonClicked(_ sender: UIButton) {
+        animateButton(sender)
+    }
+    
+    func animateButton(_ buttonToAnimate: UIView) {
+         UIView.animate(withDuration: 0.15, delay: 0, usingSpringWithDamping: 0.2, initialSpringVelocity: 0.5, options: .curveEaseIn, animations: {
+                  buttonToAnimate.transform = CGAffineTransform(scaleX: 0.92, y: 0.92)
+              }) { (_) in
+                  UIView.animate(withDuration: 0.15, delay: 0, usingSpringWithDamping: 0.4, initialSpringVelocity: 2, options: .curveEaseIn, animations: {
+                      buttonToAnimate.transform = CGAffineTransform(scaleX: 1, y: 1)
+                  }, completion: nil)
+              }
+     }
+    
 }
 
